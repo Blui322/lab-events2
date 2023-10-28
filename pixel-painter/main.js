86b4ec4d2903e4015bc87a8d91c4906af0fdced1
jd richards <jdrichardstech@gmail.com>
2023-10-28 15:54:47 -0400

    update code

---
+++ pixel-painter/main.js
@@ -0,0 +1,8 @@
+// Do not change the code below.
+// Draw Grid
+const main = document.querySelector("main");
+for (let i = 0; i < 100; i++) {
+  const div = document.createElement("div");
+  div.classList.add("cell");
+  main.append(div);
+}
